460da3244b29a40c32a858e7c493406c47fe3528
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-05-08 00:27:02 +0200

    add message with img bonus

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      message:
+        "Il mio tesoro...? Se lo volete, è vostro... cercatelo! Ho lasciato tutto in un certo posto.",
+      image: "Roger.webp",
+    };
+  },
+});
+app.mount("#root");
